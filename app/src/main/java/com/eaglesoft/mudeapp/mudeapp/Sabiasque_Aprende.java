1c42013b3df97cc8b163ea6b30a1bab3b698c640
VicenteHdz <vhs_matrix@hotmail.com>
2015-03-28 15:31:46 -0600

    El menu listo el diseño y redireccionamiento

--- app/src/main/java/com/eaglesoft/mudeapp/mudeapp/Sabiasque_Aprende.java
+++ app/src/main/java/com/eaglesoft/mudeapp/mudeapp/Sabiasque_Aprende.java
@@ -20,7 +20,7 @@ public class Sabiasque_Aprende extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_sabiasque__aprende, menu);
         return true;
-        //gjjhkhj
+
     }
 
     @Override
